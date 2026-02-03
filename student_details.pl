f8722cfaa48fe688cdedc2f3e602c5a7b1518dc8
Jumana Sathik <jumanasathik4002@gmail.com>
2026-02-03 18:04:59 +0530

    Student details script in Perl
    This Perl script stores and displays basic personal information of a student.

---
+++ student_details.pl
@@ -0,0 +1,17 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+
+my $name      = "Your_name";
+my $course    = "Msc_Bioinformatics";
+my $reg_no    = "25110017";
+my $phone     = "+91-9876543210";
+my $blood_grp = "O+";
+my $email     = "your_name@example.com";
+
+print "Name: $name\n";
+print "Course: $course\n";
+print "Register No: $reg_no\n";
+print "Phone Number: $phone\n";
+print "Blood Group: $blood_grp\n";
+print "Email ID: $email\n";
