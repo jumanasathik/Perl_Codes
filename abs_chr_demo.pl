2f5b745f3545a2fc5d4ebec73cb7f9ef290d9869
Jumana Sathik <jumanasathik4002@gmail.com>
2026-02-04 19:42:38 +0530

    Demonstrate abs and chr functions in Perl
    This program calculates absolute value and converts ASCII numbers to characters.
    It shows how Perl handles numeric and character transformations.

---
+++ abs_chr_demo.pl
@@ -0,0 +1,6 @@
+#!/usr/bin/perl
+$number = -75;
+print "Absolute value of $number is: ", abs($number), "\n";
+
+$ascii_value = 67;
+print "Character for ASCII value $ascii_value is: ", chr($ascii_value), "\n";
